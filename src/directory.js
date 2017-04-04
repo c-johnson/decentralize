eb80bcf897851e61ae647ee82a3d680641bf6f54
Chris Johnson <chris@cjohnson.io>
2017-04-03 17:47:06 -0700

    Basic dropdown working

--- src/directory.js
+++ src/directory.js
@@ -5,6 +5,9 @@ export class Directory extends Component {
   render() {
     const projects = [{
       name: 'Camlistore',
+      homepage: "https://camlistore.org",
+      type: "Protocol",
+      lastUpdated: "January 2017",
     }, {
       name: 'Camlistore',
     }, {
@@ -13,7 +16,7 @@ export class Directory extends Component {
 
     const childrenItems = projects.map((proj, index) => {
       return (
-        <DirectoryItem name={proj.name} key={index} />
+        <DirectoryItem proj={proj} key={index} />
       );
     });
 
@@ -28,10 +31,49 @@ export class Directory extends Component {
 }
 
 class DirectoryItem extends Component {
+  constructor(args) {
+    super(...args);
+
+    this.state = {
+      active: false,
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleHeaderClick = this.handleHeaderClick.bind(this);
+  }
+
+  handleHeaderClick(e) {
+    this.setState({
+      active: !this.state.active,
+    });
+  }
+
   render() {
+    const triangle = this.state.active ? "▾" : "▸";
+    const hideClass = this.state.active ? "" : " hide";
+
     return (
       <li className="directory-list-item">
-        <h3>{this.props.name}</h3>
+        <div className="directory-list-item-header" onClick={this.handleHeaderClick}>
+          <div className="directory-list-dropdown-arrow">
+            <span>{triangle}</span>
+          </div>
+          <div className="directory-list-name">
+            <h3>{this.props.proj.name}</h3>
+            <div>{this.props.proj.homepage}</div>
+          </div>
+          <div className="directory-list-type">
+            <h3>Type</h3>
+            <div>{this.props.proj.type}</div>
+          </div>
+          <div className="directory-list-last-updated">
+            <h3>Last updated</h3>
+            <div>{this.props.proj.lastUpdated}</div>
+          </div>
+        </div>
+        <div className={`directory-list-body${hideClass}`}>
+          <h1>Sup, {this.props.proj.name} fam</h1>
+        </div>
       </li>
     );
   }
